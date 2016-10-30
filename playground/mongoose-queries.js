6581fdfd533c9d252a6271a265f3da24a8278995
Timothy Recker <Timothy@Timothys-MBP.home>
2016-10-29 18:54:19 -0700

    Added queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,39 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose.js');
+const {Todo} = require('./../server/models/todo.js');
+const {User} = require('./../server/models/user.js');
+
+// var id = '58154d940063a2cd24f439af11';
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo by Id', todo);
+// }).catch((e) => console.log(e));
+
+var id = '58153668ece390f714d96844'
+
+User.findById(id).then((user) => {
+  if (!user) {
+    return console.log('Id not found');
+  }
+  console.log(user);
+}).catch((e) => console.log(e));
